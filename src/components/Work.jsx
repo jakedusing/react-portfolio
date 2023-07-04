de95704bb6ff9491ed3e2b7d368841203b0bbf03
Jake Dusing <dusingjake@gmail.com>
2023-07-03 20:08:50 -0500

    add url and change style

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -22,6 +22,36 @@ const Work = () => {
             style={{ backgroundImage: `url(${FoodRush})` }}
             className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center text-center items-center mx-auto content-div"
           >
+            {/* Grid Item */}
+            <div
+              style={{ backgroundImage: `url(${Netflix})` }}
+              className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center text-center mx-auto content-div"
+            >
+              {/* Hover Effects */}
+              <div className="opacity-0 group-hover:opacity-100">
+                <span className="text-2xl font-bold text-white tracking-wider">
+                  Netflix Clone
+                </span>
+                <div className="pt-8 text-center">
+                  <a
+                    href="https://jaked-netflixclone.netlify.app/"
+                    target="_blank"
+                  >
+                    <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                      Demo
+                    </button>
+                  </a>
+                  <a
+                    href="https://github.com/jakedusing/netflix-clone"
+                    target="_blank"
+                  >
+                    <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                      Code
+                    </button>
+                  </a>
+                </div>
+              </div>
+            </div>
             {/* Hover Effects */}
             <div className="opacity-0 group-hover:opacity-100">
               <span className="text-2xl font-bold text-white tracking-wider">
@@ -71,33 +101,7 @@ const Work = () => {
               </div>
             </div>
           </div>
-          {/* Grid Item */}
-          <div
-            style={{ backgroundImage: `url(${Netflix})` }}
-            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center text-center mx-auto content-div"
-          >
-            {/* Hover Effects */}
-            <div className="opacity-0 group-hover:opacity-100">
-              <span className="text-2xl font-bold text-white tracking-wider">
-                Netflix Clone
-              </span>
-              <div className="pt-8 text-center">
-                <a
-                  href="https://jaked-netflixclone.netlify.app/"
-                  target="_blank"
-                >
-                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
-                    Demo
-                  </button>
-                </a>
-                <a href="/">
-                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
-                    Code
-                  </button>
-                </a>
-              </div>
-            </div>
-          </div>
+
           {/* Grid Item */}
           <div
             style={{ backgroundImage: `url(${Experiences})` }}
