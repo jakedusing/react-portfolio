d1fea22108f68482bbea9626ac3edd37ebfafca7
Jake Dusing <dusingjake@gmail.com>
2023-07-06 13:37:42 -0500

    add work and favicon

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -5,6 +5,8 @@ import Netflix from "../assets/netflix.jpg";
 import Experiences from "../assets/experiences.jpg";
 import YelpCamp from "../assets/yelpcamp.png";
 import Beaches from "../assets/beaches.jpg";
+import Crypto from "../assets/crypto.jpg";
+import Finance from "../assets/finance.jpg";
 
 const Work = () => {
   return (
@@ -19,6 +21,36 @@ const Work = () => {
 
         {/* Container */}
         <div className="grid sm:grid-cols-2 gap-4">
+          {/* Grid Item */}
+          <div
+            style={{ backgroundImage: `url(${Crypto})` }}
+            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center text-center mx-auto content-div"
+          >
+            {/* Hover Effects */}
+            <div className="opacity-0 group-hover:opacity-100">
+              <span className="text-2xl font-bold text-white tracking-wider">
+                CryptoKeeper
+              </span>
+              <div className="pt-8 text-center">
+                <a
+                  href="https://jaked-cryptokeeper.netlify.app/"
+                  target="_blank"
+                >
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    Demo
+                  </button>
+                </a>
+                <a
+                  href="https://github.com/jakedusing/cryptokeeper"
+                  target="_blank"
+                >
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    Code
+                  </button>
+                </a>
+              </div>
+            </div>
+          </div>
           {/* Grid Item */}
           <div
             style={{ backgroundImage: `url(${Netflix})` }}
@@ -179,6 +211,30 @@ const Work = () => {
                   </button>
                 </a>
                 <a href="https://github.com/jakedusing/beaches" target="_blank">
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    Code
+                  </button>
+                </a>
+              </div>
+            </div>
+          </div>
+          {/* Grid Item */}
+          <div
+            style={{ backgroundImage: `url(${Finance})` }}
+            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center text-center mx-auto content-div"
+          >
+            {/* Hover Effects */}
+            <div className="opacity-0 group-hover:opacity-100">
+              <span className="text-2xl font-bold text-white tracking-wider">
+                REACT Finance
+              </span>
+              <div className="pt-8 text-center">
+                <a href="https://jaked-finances.netlify.app/" target="_blank">
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    Demo
+                  </button>
+                </a>
+                <a href="https://github.com/jakedusing/finance" target="_blank">
                   <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
                     Code
                   </button>
